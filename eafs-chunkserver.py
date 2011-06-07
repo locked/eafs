55f7d59ddc6b93bfc138197514f7ab0237cf2201
Adam Etienne <eadam@lunasys.fr>
2011-06-07 13:17:01 +0200

    add license

--- eafs-chunkserver.py
+++ eafs-chunkserver.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+ * EAFS
+ * Copyright (C) 2009-2011 Adam Etienne <eadam@lunasys.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import math,uuid,os,time,operator,sys,argparse
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
